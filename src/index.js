6518e088dfd4e5486892c4cd61ee91997d4df62d
Mrcode <734202639@qq.com>
2016-08-10 16:34:50 +0800

    resize window size

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 400, height: 300})
+  win = new BrowserWindow({width: 700, height: 450})
 
   // and load the index.html of the app.
   win.loadURL(`file://${path.join(__dirname, '..')}/index.html`)
